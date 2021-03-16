c198eec75c3b04ae7fcb039c0365bc39f57e763d
Darnel Castore <dcastor@student.clayton.edu>
2021-03-16 17:16:49 -0400

    add game.py and main.py script

---
+++ main.py
@@ -0,0 +1,10 @@
+from classes.game import Person
+
+magic = [{"name": "Fire", "Cost":10, "dmg":25},
+         {"name": "Thunder", "Cost":10, "dmg":60},
+         {"name": "Blizzard", "Cost":15, "dmg":30}]
+
+
+player = Person(460, 65, 60, 36, magic)
+
+print(player.generate_damage())
