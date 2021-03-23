37ab6d413bb58878f9b1cb68e6eeb48d4c6b5186
Darnel Castore <dcastor@student.clayton.edu>
2021-03-22 23:26:57 -0400

    fixing AttributeError on line 24

--- classes/game.py
+++ classes/game.py
@@ -37,6 +37,10 @@ class Person:
         if self.hp < 0:
             self.hp = 0
         return self.hp
+    def heal(self,dmg):
+        self.hp -= dmg
+        if self.hp > self.maxhp:
+            self.hp = self.maxhp
 
     def get_hp(self):
         return self.hp
@@ -54,25 +58,25 @@ class Person:
         self.mp - cost
 
 
-def get_spell_name(self, i):
-    return self.magic[i]["name"]
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
 
 
-def get_spell_mp_cost(self, i):
-    return self.magic[i]["cost"]
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
 
 
-def choose_action(self):
-    i = 1
-    print("Actions")
-    for item in self.actions:
-        print(str(i) + ":", item)
-        i += 1
+    def choose_action(self):
+        i = 1
+        print("Actions")
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i += 1
 
 
-def choose_magic_spell(self):
-    i = 1
-    print("Magic")
-    for spell in self.magic:
-        print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
-        i += 1
+    def choose_magic_spell(self):
+        i = 1
+        print("Magic")
+        for spell in self.magic:
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
+            i += 1
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from classes.game import Person,bbcolors
+from classes.game import Person, bcolors
 
 magic = [{"name": "Fire", "Cost":10, "dmg":25},
          {"name": "Thunder", "Cost":10, "dmg":60},
@@ -6,5 +6,25 @@ magic = [{"name": "Fire", "Cost":10, "dmg":25},
 
 
 player = Person(460, 65, 60, 36, magic)
+enemy= Person(1200,65,45,25, magic)
 
-print(player.generate_damage())
+running = True
+i= 0
+print(bcolors.FAIL+ bcolors.BOLD + "AN ENEMY ATTACKS" + bcolors.ENDC + "this is normal")
+
+while running:
+    print("===============================")
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) -1
+
+    if  index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print ("your attacked  for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+
+    enemy_choice = 1
+
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for",enemy_dmg, "Player HP",player.get_hp())
