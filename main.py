13023a501b7f425be647e62ca89f473a7aa1c143
Darnel Castore <dcastor@student.clayton.edu>
2022-05-08 22:37:51 -0400

    Section 4:lesson39

--- main.py
+++ main.py
@@ -43,16 +43,6 @@ while running:
 
     enemy_dmg = enemy.generate_damage()
     player.take_damage(enemy_dmg)
-<<<<<<< HEAD
-    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
-
-    if enemy.get_hp() == 0:
-        print(bcolors.OKGREEN + "You Win!" + bcolors.ENDC)
-        running = False
-    elif player.get_hp() == 0:
-        print(bcolors.FAIL + "your enemy has defeated you!" + bcolors.ENDC)
-        running = False
-=======
     print("Enemy attacks for",enemy_dmg, "Player HP",player.get_hp())
     
     if enemy.gethp()== 0:
@@ -62,4 +52,3 @@ while running:
     elif player.get_hp() == 0:
         print(bcolors.FAIL + "You lose!" + bcolors.ENDC)
         
->>>>>>> aaff70fa87e769abb002a1053ffc7116f99b09e2
